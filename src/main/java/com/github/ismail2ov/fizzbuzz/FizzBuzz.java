b567bfc07eb395d9bb3ef7df4f34b0a9e1f5cb9c
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-27 08:22:02 +0200

    Convert 2 to string 2

--- src/main/java/com/github/ismail2ov/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/github/ismail2ov/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.github.ismail2ov.fizzbuzz;
 
 public class FizzBuzz {
     public String convert(int number) {
+        if (number == 2) {
+            return "2";
+        }
         return "1";
     }
 }
--- src/test/java/com/github/ismail2ov/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/com/github/ismail2ov/fizzbuzz/FizzBuzzShould.java
@@ -10,4 +10,9 @@ public class FizzBuzzShould {
     void convert_1_to_string_1() {
         assertThat(new FizzBuzz().convert(1)).isEqualTo("1");
     }
+
+    @Test
+    void convert_2_to_string_2() {
+        assertThat(new FizzBuzz().convert(2)).isEqualTo("2");
+    }
 }
