57dded6bda29a9d8ec63f247f8adffa1df7a6b18
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-27 08:24:32 +0200

    Refactor: move object creation to set up method

--- src/test/java/com/github/ismail2ov/fizzbuzz/FizzBuzzShould.java
+++ src/test/java/com/github/ismail2ov/fizzbuzz/FizzBuzzShould.java
@@ -1,18 +1,26 @@
 package com.github.ismail2ov.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzShould {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        this.fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void convert_1_to_string_1() {
-        assertThat(new FizzBuzz().convert(1)).isEqualTo("1");
+        assertThat(this.fizzBuzz.convert(1)).isEqualTo("1");
     }
 
     @Test
     void convert_2_to_string_2() {
-        assertThat(new FizzBuzz().convert(2)).isEqualTo("2");
+        assertThat(this.fizzBuzz.convert(2)).isEqualTo("2");
     }
 }
